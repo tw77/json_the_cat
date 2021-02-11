dcda64354dc4f722a7ac6d8bee24bca8ca4086cc
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-11 23:34:27 +0000

    Refactor with modules

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,12 @@
 const request = require('request');
-const breedName = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
-  if (error) return console.log(error);
-  const data = JSON.parse(body);
-  if (data.length === 0) console.log('Sorry, requested breed not found.');
-  else console.log(data[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2], (error, response, body) => {
+    if (error) callback(error, null);
+    const data = JSON.parse(body);
+    if (data.length === 0) callback(null, 'Sorry, requested breed not found.');
+    if (data.length > 0) callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
