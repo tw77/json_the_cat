466c7e434dbb1b0b21402c5aafbb29885f707342
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-11 23:57:32 +0000

    Completed with tests implemented

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,9 @@
 const request = require('request');
+// const breedName = process.argv[2];
+
 
 const fetchBreedDescription = function(breedName, callback) {
-  request('https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2], (error, response, body) => {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
     if (error) callback(error, null);
     const data = JSON.parse(body);
     if (data.length === 0) callback(null, 'Sorry, requested breed not found.');
