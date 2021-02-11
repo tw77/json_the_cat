e8c88c973df85a47e7e92c778f2f5073fdf888f6
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-11 22:35:53 +0000

    Fixed error message logic

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,6 +2,7 @@ const args = process.argv;
 
 const request = require('request');
 request('https://api.thecatapi.com/v1/breeds/search?q=' + args[2], (error, response, body) => {
+  if (error) return console.log(error);
   const data = JSON.parse(body);
   if (data.length === undefined) console.log(data.message);
   else if (data.length === 0) console.log('Sorry, requested breed not found.');
